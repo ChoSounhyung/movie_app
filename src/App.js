4935e1ab49a9cef2610aada01108eb33a6a6d710
ChoSounhyung <s2019w36@e-mirim.hs.kr>
2021-03-13 01:23:09 +0900

    Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import Potato from "./potato";
+
+function Food({ favorite }) {
+  return <h1>I like {favorite}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Potato />
+      <Food favorite="kimchi" />
+      <Food favorite="ramen" />
+      <Food favorite="samgioqsal" />
+      <Food favorite="chukumi" />
     </div>
   );
 }
